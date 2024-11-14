90ea0ca5409f7d46398497d0114a6e22ed8b6667
Dulinniel <nightingalenervous@gmail.com>
2024-11-14 03:22:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tools
+
+It just compîle tools I use daily, I will update it based on my need or on request. Bash files may not be executable so don't forget to : `chmod +x ./<filename>.sh` the first time you use them
+
+## List
+
+- [videoConverter](#videoConverter)
+
+### videoConverter
+
+A utility that use raw ffmpeg to convert a video format to another
+
+Basic usage : 
+
+```bash
+./videoConverter.sh -i input.mp4 -o mkv
+```
+
+By default, the file you converted get moved to you current working directory. You can override this behaviour by specifying a folder to move the output file to, using the `-d` parameter such as:
+
+```bash
+./videoConverter.sh -i input.mp4 -o mkv -d ~/Videos
+```
+
+Now after the conversion, the file will automatically moved to Videos folder. Furthermore, you can automatically remove the input file from your system, by adding `-p` parameter to the command like : 
+
+```bash
+./videoConverter.sh -i input.mp4 -o mkv -p
+```
---
+++ videoConverter.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+AUDIO_CODEC=libvorbis
+VIDEO_CODEC=vp9
+
+VERSION="1.0.0"
+AUTHOR="Dulinniel"
+SOURCE="https://github.com/Dulinniel/Tools"
+PURGE=false
+DESTINATION=`pwd`
+INPUT=None
+OUTPUT=None
+
+# Help function, display simple usage
+function help()
+{
+  echo "usage: videoConverter -i /path/to/file -o video_extension [-d /path/to/output/dir] [-p]"
+  echo "-i      Set the input file to convert"
+  echo "-o      Set the video extension to convert to"
+  echo "-d      If present, automatically move the output file to the desired location"
+  echo "-p      If present, automatically delete the input file"
+  echo "-h      Display this help"
+  echo "-v      Display application version"
+  echo "-a      Display the name of the author"
+  echo "-s      Display the link to the source code"
+}
+
+# Display the tool version
+function version()
+{
+  echo "$VERSION"
+}
+
+# Display the author name
+function author()
+{
+  echo "$AUTHOR"
+}
+
+# Display the link to the source of the tool
+function source()
+{
+  echo "$SOURCE"
+}
+
+# Parse short options
+OPTIND=1
+while getopts "d:i:o:p:hvas" opt
+do
+  case "$opt" in
+    'i') INPUT="$OPTARG";;
+    'd') DESTINATION="$OPTARG";;
+    'o') OUTPUT="$OPTARG";;
+    'p') PURGE=true;;
+    'h') help; exit 0 ;;
+    'v') version; exit 0 ;;
+    'a') author; exit 0 ;;
+    's') source; exit 0 ;;
+  esac
+done
+
+# Remove options from positional parameters
+shift $(expr $OPTIND - 1) 
+
+# Check if the file is usable
+if [ ! -f $INPUT ] || [ ! -e $INPUT ] 
+then
+  echo "File does not exist"
+  exit -1
+fi
+
+# Concatenate input file name with output video extension
+FINAL_FILE="${INPUT%%.*}.$OUTPUT"
+# Convert it to output file using vp9 video codec and libvorbis video codec
+ffmpeg -i $INPUT -c:v $VIDEO_CODEC -c:a $AUDIO_CODEC $FINAL_FILE
+# Move the output file to the destination
+mv $FINAL_FILE $DESTINATION
+# If present, remove the original file
+if [ $PURGE ] 
+then
+  rm -rf $INPUT
+fi
